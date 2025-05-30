713e345a447546e9b51cc21c1e5d20da610b6be8
Rodolfo Espinosa-Nunez <rodolfoespinosa01@gmail.com>
2025-05-30 12:44:45 -0400

    completed challenge chapter 2

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,28 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+interface Todo {
+    id: number
+    title: string
+    status: TodoStatus
+    completedOn?: Date
+}
+
+enum TodoStatus {
+    Todo = "todo",
+    InProgress = "in-progress",
+    Done ="done"
+}
+const todoItems: Todo[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoStatus.Todo },
 ]
 
-function addTodoItem(todo) {
+function addTodoItem(todo: string): Todo {
     const id = getNextId(todoItems)
 
     const newTodo = {
         id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.Todo,
     }
 
     todoItems.push(newTodo)
@@ -18,7 +30,7 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+function getNextId<T extends { id: number }>(items: T[]) {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
 
